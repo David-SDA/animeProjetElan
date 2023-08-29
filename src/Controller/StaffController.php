4f9479317348ce6a2d64c923f18027503cba07c7
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-08-29 11:59:37 +0200

    Début d'ajout données API, page un membre de staff

--- src/Controller/StaffController.php
+++ src/Controller/StaffController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\StaffCallApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,7 +18,9 @@ class StaffController extends AbstractController
     }
 
     #[Route('/staff/{id}', name: 'show_staff')]
-    public function show(int $id): Response{
-        return $this->render('staff/show.html.twig');
+    public function show(int $id, StaffCallApiService $staffCallApiService): Response{
+        return $this->render('staff/show.html.twig', [
+            'dataOneStaff' => $staffCallApiService->getStaffDetails($id),
+        ]);
     }
 }
--- src/Service/StaffCallApiService.php
+++ src/Service/StaffCallApiService.php
@@ -11,5 +11,58 @@ class StaffCallApiService{
         $this->client = $client;
     }
 
+    /**
+     * Fonction qui permet d'obtenir les informations d'un membre de staff
+     */
+    public function getStaffDetails(int $id){
+        // Définition de la query
+        $query = '
+            query($id: Int){
+                Staff(id: $id){
+                    id
+                    name{
+                        full
+                    }
+                    image{
+                        large
+                    }
+                    age
+                    gender
+                    description
+                    primaryOccupations
+                    staffMedia(type: ANIME){
+                        edges{
+                            staffRole
+                            roleNotes
+                            node{
+                                id
+                                title {
+                                    romaji
+                                }
+                                coverImage{
+                                    large
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+          
+        ';
 
+        // Définition des variables utilisées dans la query
+        $variables = [
+            'id' => $id,
+        ];
+
+        // Appel à l'API
+        $response = $this->client->request('POST', 'https://graphql.anilist.co', [
+            'json' => [
+                'query' => $query,
+                'variables' => $variables,
+            ]
+        ]);
+
+        return $response->toArray(); // On retourne les données sous forme de tableau
+    }
 }
