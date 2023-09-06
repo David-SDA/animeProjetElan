70088040bbe9e75103437256c3fc3ca5163ba036
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-09-06 11:59:25 +0200

    Début de la page de paramètres d'un utilisateur

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,6 +7,7 @@ use App\Service\HomeCallApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class UserController extends AbstractController
 {
@@ -23,6 +24,16 @@ class UserController extends AbstractController
         return $this->render('user/show.html.twig',  [
             'user' => $user,
             'dataTopTenAnime' => $homeCallApiService->getTopTenAnime(), // Test visuel uniquement, à retirer
+        ]);
+    }
+
+    #[Route('user/{id}/settings', name: 'settings_user')]
+    public function settings(User $user): Response{
+        if($this->getUser() !== $user){
+            throw new AccessDeniedException();
+        }
+        return $this->render('user/settings.html.twig', [
+            'user' => $user,
         ]);
     }
 }
