f83209db83c501590313bd211cd0465bd082778a
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-09-06 09:44:29 +0200

    Ajout partie des animés favoris, à modifier later

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Service\HomeCallApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,9 +19,10 @@ class UserController extends AbstractController
     }
 
     #[Route('/user/{id}', name: 'show_user')]
-    public function show(User $user): Response{
+    public function show(User $user, HomeCallApiService $homeCallApiService): Response{
         return $this->render('user/show.html.twig',  [
             'user' => $user,
+            'dataTopTenAnime' => $homeCallApiService->getTopTenAnime(), // Test visuel uniquement, à retirer
         ]);
     }
 }
