692b0468a924ebe12b68bfe4e083215ee44a0df9
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-09-11 15:46:29 +0200

    Modif, obtention des données de l'API pour liste

--- src/Controller/UserAnimeListController.php
+++ src/Controller/UserAnimeListController.php
@@ -14,50 +14,48 @@ class UserAnimeListController extends AbstractController
 {
     #[Route('user/{id}/animeList', name: 'anime_list_user')]
     public function animeList(User $user, AnimeCallApiService $animeCallApiService): Response{
-        /* Création d'une variable pour obtenir les animés dans la liste de l'utilisateur */
-        $userAnimes = $user->getUserRegarderAnimes();
-
-        /* Tableaux d'animes dans la liste de l'utilisateur en fonction de leur état */
-        $animesWatching = [];
-        $animesCompleted = [];
-        $animesPlanned = [];
-
-        /* On ajoute les animé dans les différents tableaux en fonction de leur état */
-        foreach($userAnimes as $userAnime){
-            $etat = $userAnime->getEtat();
-
-            if($etat === 'Watching'){
-                $animesWatching[] = $userAnime;
+        /* On crée un tableau avec les 3 états possible d'un animé dans une liste */
+        $animeByState = [
+            'Watching' => [],
+            'Completed' => [],
+            'Plan to watch' => [],
+        ];
+    
+        /* Pour chaque instance dans la liste d'un utilisateur, on intégre au tableau l'anime de la base de données concerné en fonction de son état  */
+        foreach($user->getUserRegarderAnimes() as $userAnime){
+            $state = $userAnime->getEtat();
+            $animeByState[$state][] = $userAnime;
+        }
+        
+        /* Création de la variable pour les données des animés */
+        $animeDataByState = [];
+    
+        /* Pour chaque état dans le tableau ($state => $userAnimes : permet d'avoir accès aux clés, ici l'état) */
+        foreach($animeByState as $state => $userAnimes){
+            /* On crée un tableau d'ids (ids pour l'API) */
+            $animeIds = [];
+            
+            /* Pour chaque animé d'un état */
+            foreach ($userAnimes as $userAnime){
+                /* On stock l'id de l'API dans le tableau créer précedement */
+                $animeIds[] = $userAnime->getAnime()->getIdApi();
             }
-            elseif($etat === 'Completed'){
-                $animesCompleted[] = $userAnime;
+            
+            /* On récupère les données de l'API grâce au tableau d'id */
+            $animeData = $animeCallApiService->getAnimesFromList($animeIds);
+    
+            /* On crée un tableau associatifs entre état et donnée de l'API */
+            $animeDataByState[$state] = [];
+    
+            /* Pour chaque animé dans la liste d'un utilisateur, on ajoute au tableau tout juste créé l'id de l'instance de la liste et les données de l'API */
+            foreach ($userAnimes as $userAnime){
+                $animeDataByState[$state][$userAnime->getId()] = $animeData;
             }
-            else{
-                $animesPlanned[] = $userAnime;
-            }
         }
-
-        /* Pour chaque tableau d'animés, on récupère l'id des animés (servira à l'appel de l'API) */
-        $animesWatchingApiIds = [];
-        foreach($animesWatching as $anime){
-            $animesWatchingApiIds[] = $anime->getAnime()->getIdApi();
-        }
-
-        $animesCompletedApiIds = [];
-        foreach($animesCompleted as $anime){
-            $animesCompletedApiIds[] = $anime->getAnime()->getIdApi();
-        }
-
-        $animesPlannedApiIds = [];
-        foreach($animesPlanned as $anime){
-            $animesPlannedApiIds[] = $anime->getAnime()->getIdApi();
-        }
-
+        
         return $this->render('user_anime_list/animeList.html.twig', [
             'user' => $user,
-            'animesWatching' => $animeCallApiService->getAnimesFromList($animesWatchingApiIds),
-            'animesCompleted' => $animeCallApiService->getAnimesFromList($animesCompletedApiIds),
-            'animesPlanned' => $animeCallApiService->getAnimesFromList($animesPlannedApiIds),
+            'animesDataByState' => $animeDataByState,
         ]);
     }
 
@@ -78,7 +76,6 @@ class UserAnimeListController extends AbstractController
             'status' => $userRegarderAnime_id->getEtat(),
             'numberEpisodes' => $userRegarderAnime_id->getNombreEpisodeVu(),
         ]);
-
         return $this->render('user_anime_list/modifyAnimeList.html.twig', [
             'form' => $form->createView(),
         ]);
