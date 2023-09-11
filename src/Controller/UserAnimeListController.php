dcbfc19df0b4af9b1524aa1ff47785cc9f6ece7a
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-09-11 09:56:54 +0200

    Ajout séparation animés en fonction de leur état

--- src/Controller/UserAnimeListController.php
+++ src/Controller/UserAnimeListController.php
@@ -12,14 +12,52 @@ class UserAnimeListController extends AbstractController
 {
     #[Route('user/{id}/animeList', name: 'anime_list_user')]
     public function animeList(User $user, AnimeCallApiService $animeCallApiService): Response{
+        /* Création d'une variable pour obtenir les animés dans la liste de l'utilisateur */
+        $userAnimes = $user->getUserRegarderAnimes();
 
-        $animes = $user->getUserRegarderAnimes()->map(function ($anime){
+        $animes = $userAnimes->map(function ($anime){
             return $anime->getAnime()->getIdApi();
         })->toArray();
 
+        /* Tableaux d'animes dans la liste de l'utilisateur en fonction de leur état */
+        $animesWatching = [];
+        $animesCompleted = [];
+        $animesPlanned = [];
+
+        foreach($userAnimes as $userAnime){
+            $etat = $userAnime->getEtat();
+
+            if($etat === 'WATCHING'){
+                $animesWatching[] = $userAnime;
+            }
+            elseif($etat === 'COMPLETED'){
+                $animesCompleted[] = $userAnime;
+            }
+            else{
+                $animesPlanned[] = $userAnime;
+            }
+        }
+
+        $animesWatchingApiIds = [];
+        foreach($animesWatching as $anime){
+            $animesWatchingApiIds[] = $anime->getAnime()->getIdApi();
+        }
+
+        $animesCompletedApiIds = [];
+        foreach($animesCompleted as $anime){
+            $animesCompletedApiIds[] = $anime->getAnime()->getIdApi();
+        }
+
+        $animesPlannedApiIds = [];
+        foreach($animesPlanned as $anime){
+            $animesPlannedApiIds[] = $anime->getAnime()->getIdApi();
+        }
+
         return $this->render('user_anime_list/animeList.html.twig', [
             'user' => $user,
-            'animes' => $animeCallApiService->getAnimesFromList($animes),
+            'animesWatching' => $animeCallApiService->getAnimesFromList($animesWatchingApiIds),
+            'animesCompleted' => $animeCallApiService->getAnimesFromList($animesCompletedApiIds),
+            'animesPlanned' => $animeCallApiService->getAnimesFromList($animesPlannedApiIds),
         ]);
     }
 }
