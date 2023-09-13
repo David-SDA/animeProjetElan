26545a16b47c7b2a13de4416761c95fb5bda7a33
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-09-13 11:38:13 +0200

    Modif affichage bouton ajout animé aux favoris

--- src/Controller/AnimeController.php
+++ src/Controller/AnimeController.php
@@ -42,16 +42,34 @@ class AnimeController extends AbstractController
 
     #[Route('/anime/{id}', name: 'show_anime')]
     public function show(int $id, AnimeCallApiService $animeCallApiService, UserRegarderAnimeRepository $userRegarderAnimeRepository, AnimeRepository $animeRepository): Response{
+        /* On recupère l'utilisateur actuel */
+        $currentUser = $this->getUser();
+        
         /* On crée une variable pour déterminer si l'animé est dans la liste de l'utilisateur, on définit que de base, il n'y est pas */
         $animeIsInList = false;
         /* Si l'utilisateur est connecté et que l'on trouve une instance de userRegarderAnime avec le même user et le même anime (que l'on cherche aussi) */
-        if($this->getUser() && $userRegarderAnimeRepository->findOneBy(['user' => $this->getUser(), 'anime' => $animeRepository->findOneBy(['idApi' => $id])])){
+        if($currentUser && $userRegarderAnimeRepository->findOneBy(['user' => $currentUser, 'anime' => $animeRepository->findOneBy(['idApi' => $id])])){
             /* Alors l'utilisateur a déjà l'anime dans sa liste */
             $animeIsInList = true;
         }
+
+        /* On crée une variable pour déterminer si l'animé est dans les animés favoris de l'utilisateur, on définit que de base, il n'y est pas */
+        $animeIsInFavorites = false;
+
+        /* Pour chaque animés dans la collection d'un animés de l'utilisateur (qui représente les animés favoris de l'utilisateur) */
+        foreach($currentUser->getAnimes() as $anime){
+            /* On vérifie si l'id de l'API correspond à l'id de l'animé de la page actuelle */
+            if($anime->getIdApi() === $id){
+                /* Dès que l'on trouve une correspondance, cela veut dire que l'animé est bien dans les animés favoris de l'utilisateur et on stop la boucle dès que cela arrive */
+                $animeIsInFavorites = true;
+                break;
+            }
+        }
+
         return $this->render('anime/show.html.twig', [
             'dataOneAnime' => $animeCallApiService->getAnimeDetails($id),
             'animeIsInList' => $animeIsInList,
+            'animeIsInFavorites' => $animeIsInFavorites,
         ]);
     }
 
