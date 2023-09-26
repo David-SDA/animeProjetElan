10d516d246a9fa1b7a723ac8c39ef1fd68904475
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-09-26 15:10:43 +0200

    Ajout de la base de la création d'une discussion

--- src/Controller/DiscussionController.php
+++ src/Controller/DiscussionController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Discussion;
+use App\Form\DiscussionFormType;
 use App\Repository\DiscussionRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DiscussionController extends AbstractController
 {
@@ -18,6 +21,30 @@ class DiscussionController extends AbstractController
 
         return $this->render('discussion/index.html.twig', [
             'talks' => $talks,
+        ]);
+    }
+
+    #[Route('/discussion/add', name: 'add_discussion')]
+    #[Route('/discussion/{id}/edit', name:'edit_discussion')]
+    public function add(EntityManagerInterface $entityManagerInterface, Discussion $discussion = null, Request $request): Response{
+        /* On récupère l'utilisateur actuel */
+        $user = $this->getUser();
+        /* Si l'utilisateur n'est pas connecté, on l'empeche de créer des discussions */
+        if(!$user){
+            return $this->redirectToRoute('app_home');
+        }
+
+        /* Si la discussion n'existe pas, on la crée */
+        if(!$discussion){
+            $discussion = new Discussion();
+        }
+
+        /* Création du formulaire */
+        $form = $this->createForm(DiscussionFormType::class);
+
+        return $this->render('discussion/add.html.twig', [
+            'form' => $form->createView(),
+            'discussion' => $discussion->getId(),
         ]);
     }
 
---
+++ src/Form/DiscussionFormType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class DiscussionFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre', TextType::class, [
+                'required' => true,
+            ])
+            ->add('firstPost', TextareaType::class, [
+                'required' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
