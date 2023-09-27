c5c3189f740d3b9f23e0074637c0052e2e820da2
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-09-27 15:11:48 +0200

    Ajout de l'ajout d'un post à une discussion

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Discussion;
+use App\Entity\Post;
+use App\Form\PostFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,6 +18,50 @@ class PostController extends AbstractController
     {
         return $this->render('post/index.html.twig', [
             'controller_name' => 'PostController',
+        ]);
+    }
+
+    #[Route('/discussion/{id}/post/add', name: 'add_post')]
+    public function add(EntityManagerInterface $entityManagerInterface, Discussion $discussion, Request $request): Response{
+        /* On récupère l'utilisateur actuel */
+        $user = $this->getUser();
+        /* Si l'utilisateur n'est pas connecté, on l'empeche de créer des discussions */
+        if(!$user || $discussion->isEstVerrouiller()){
+            return $this->redirectToRoute('app_home');
+        }
+
+        /* On crée un nouveau post */
+        $post = new Post();
+
+        /* Création du formulaire */
+        $form = $this->createForm(PostFormType::class);
+        /* Vérification de la requête qui permet de verifier si le formulaire est soumis */
+        $form->handleRequest($request);
+        
+        /* Si le formulaire est soumis et est valide (données entrées sont correct) */
+        if($form->isSubmitted() && $form->isValid()){
+            $post->setDateCreation(new \DateTime());
+            $post->setDateDerniereModification(new \DateTime());
+            $post->setContenu($form->get('content')->getData());
+            $post->setUser($user);
+            $post->setDiscussion($discussion);
+
+            /* On sauvegarde ces changements dans la base de données */
+            $entityManagerInterface->persist($post);
+            $entityManagerInterface->flush();
+
+            /* On indique le succès de la création */
+            $this->addFlash(
+                'success',
+                'Post has been created successfully'
+            );
+
+            return $this->redirectToRoute('show_discussion', ['id' => $discussion->getId()]);
+        }
+
+        return $this->render('post/add.html.twig', [
+            'form' => $form->createView(),
+            'postExist' => false
         ]);
     }
 }
--- src/Form/PostFormType.php
+++ src/Form/PostFormType.php
@@ -2,29 +2,24 @@
 
 namespace App\Form;
 
-use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class PostFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('dateCreation')
-            ->add('dateDerniereModification')
-            ->add('contenu')
-            ->add('user')
-            ->add('users')
-            ->add('discussion')
+        ->add('content', TextareaType::class, [
+            'required' => true,
+        ])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([
-            'data_class' => Post::class,
-        ]);
+        $resolver->setDefaults([]);
     }
 }
