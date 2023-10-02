331dabf38c23e9215921f5fd26b12e481b544762
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-10-02 10:04:50 +0200

    Modif et style de la modif du formulaire évènement

--- src/Form/EvenementFormType.php
+++ src/Form/EvenementFormType.php
@@ -8,18 +8,25 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Validator\Constraints\Length;
 
 class EvenementFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nomEvenement', TextType::class)
+            ->add('nomEvenement', TextType::class, [
+                'constraints' => [
+                    new Length([
+                        'max' => 100,
+                    ])
+                ]
+            ])
             ->add('dateDebut', DateTimeType::class, [
-                'date_widget' => 'single_text'
+                'widget' => 'single_text'
             ])
             ->add('dateFin', DateTimeType::class, [
-                'date_widget' => 'single_text'
+                'widget' => 'single_text'
             ])
         ;
     }
