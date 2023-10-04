2fd22753774285aacff7bcc366569e6116bb9aac
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-10-04 10:57:58 +0200

    Ajout du débannissement d'un utilisateur

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -49,14 +49,42 @@ class AdminController extends AbstractController
             /* On indique la réussite du bannissement */
             $this->addFlash(
                 'success',
-                'The user ' . $user->getPseudo() . ' has been banned successfully'
+                'The user "' . $user->getPseudo() . '" has been banned successfully'
             );
         }
         else{
             /* On indique l'échec du bannissement */
             $this->addFlash(
                 'error',
-                'The user ' . $user->getPseudo() . ' is already banned'
+                'The user "' . $user->getPseudo() . '" is already banned'
+            );
+        }
+
+        return $this->redirectToRoute('users_admin');
+    }
+
+    #[Route('/admin/user/{id}/unban', name:'unban_user_admin')]
+    public function unban(EntityManagerInterface $entityManagerInterface, User $user){
+        /* Si l'utilisateur est banni */
+        if($user->isEstBanni()){
+            /* On le débanni */
+            $user->setEstBanni(false);
+
+            /* On sauvegarde les changements en base de données */
+            $entityManagerInterface->persist($user);
+            $entityManagerInterface->flush();
+
+            /* On indique la réussite du débannissement */
+            $this->addFlash(
+                'success',
+                'The user "' . $user->getPseudo() . '" has been unbanned successfully'
+            );
+        }
+        else{
+            /* On indique l'échec du débannissement */
+            $this->addFlash(
+                'error',
+                'The user "' . $user->getPseudo() . '" is already unbanned'
             );
         }
 
