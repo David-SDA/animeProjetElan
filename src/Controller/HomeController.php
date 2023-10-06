e0d3215b7579e1a9b4656d440e7058cdcea3e363
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-10-06 15:58:21 +0200

    Modif contrôleur home pour utilisateur banni

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,8 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(HomeCallApiService $homeCallApiService): Response
-    {
+    public function index(HomeCallApiService $homeCallApiService): Response{
+        /* Si l'utilisateur est banni, on le redirige vers la page d'un banni */
+        if($this->getUser() && $this->getUser()->isEstBanni()){
+            return $this->redirectToRoute('app_banned');
+        }
+        
         return $this->render('home/index.html.twig', [
             'dataBestAnimeThisSeason' => $homeCallApiService->getBestAnimeThisSeason(),
             'dataNextSeasonAnime' => $homeCallApiService->getNextSeasonAnime(),
