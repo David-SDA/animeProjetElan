b6b1d51abdd11fd4954416b31db694ab09175a6c
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-10-06 16:16:42 +0200

    Modif contrôleur staff pour utilisateur banni

--- src/Controller/StaffController.php
+++ src/Controller/StaffController.php
@@ -7,18 +7,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/staff')]
 class StaffController extends AbstractController
 {
-    #[Route('/staff', name: 'app_staff')]
-    public function index(): Response
-    {
-        return $this->render('staff/index.html.twig', [
-            'controller_name' => 'StaffController',
-        ]);
-    }
-
-    #[Route('/staff/{id}', name: 'show_staff')]
+    #[Route('/{id}', name: 'show_staff')]
     public function show(int $id, StaffCallApiService $staffCallApiService): Response{
+        /* Si l'utilisateur est banni, on le redirige vers la page d'un banni */
+        if($this->getUser() && $this->getUser()->isEstBanni()){
+            return $this->redirectToRoute('app_banned');
+        }
+
         return $this->render('staff/show.html.twig', [
             'dataOneStaff' => $staffCallApiService->getStaffDetails($id),
         ]);
