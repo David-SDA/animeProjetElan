0cafc2c344d9e373a6cb36da135f66b1d8a5fdde
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-10-10 11:29:03 +0200

    Ajout regex pour changer description d'un perso

--- src/Controller/CharacterController.php
+++ src/Controller/CharacterController.php
@@ -37,10 +37,29 @@ class CharacterController extends AbstractController
             }
         }
 
+        /* On récupère l'animé depuis la base de données si il existe */
         $characterInDatabase = $personnageRepository->findOneBy(['idApi' =>  $id]);
+
+        /* Récupération des données de l'API */
+        $characterDetails = $characterCallApiService->getCharacterDetails($id);
+        
+        /* Regex qui match les liens que l'on veut remplacer */
+        /* Par exemple doit des chaîne de caractères de ce genre : [Fern](https://anilist.co/character/183965) et [Fern](https://anilist.co/character/183965/Fern) */
+        $regex = '/\[(.*?)\]\(https:\/\/anilist\.co\/character\/(\d+)(?:\/\S+)?\)/';
+
+        /* Modification des lien de la description pour qu'il redirige vers les liens de notre site */
+        $characterDetails['data']['Character']['description'] = preg_replace_callback(
+            $regex,
+            function($matches){
+                $characterId = $matches[2];
+                $characterName = $matches[1];
+                return '<a href="' . $this->generateUrl('show_character', ['id' => $characterId]) . '">' . $characterName . '</a>';
+            },
+            $characterDetails['data']['Character']['description']
+        );
         
         return $this->render('character/show.html.twig', [
-            'dataOneCharacter' => $characterCallApiService->getCharacterDetails($id),
+            'dataOneCharacter' => $characterDetails,
             'characterIsInFavorites' => $characterIsInFavorites,
             'characterInDatabase' => $characterInDatabase,
         ]);
