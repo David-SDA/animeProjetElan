f0931c46ff1b1f2535edb0aadc39c72955008f7b
David Sousa de Araujo <davidaraujo2001@gmail.com>
2024-04-23 15:51:51 +0200

    Cache pour les appels à l'API dans la page d'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,21 +6,48 @@ use App\Service\HomeCallApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(HomeCallApiService $homeCallApiService): Response{
+    public function index(HomeCallApiService $homeCallApiService, CacheInterface $cache): Response{
         /* Si l'utilisateur est banni, on le redirige vers la page d'un banni */
         if($this->getUser() && $this->getUser()->isBanned()){
             return $this->redirectToRoute('app_banned');
         }
+
+        /* Récupération des données de l'API avec une mise en cache */
+        /* Meilleurs animés de cette saison */
+        $bestAnimeThisSeason = $cache->get('best_anime_this_season', function(ItemInterface $item) use($homeCallApiService){
+            $item->expiresAt(new \DateTime('tomorrow'));
+            return $homeCallApiService->getBestAnimeThisSeason();
+        });
+
+        /* Animés de la prochaine saison */
+        $nextSeasonAnime = $cache->get('next_season_anime', function(ItemInterface $item) use($homeCallApiService){
+            $item->expiresAt(new \DateTime('tomorrow'));
+            return $homeCallApiService->getNextSeasonAnime();
+        });
+
+        /* Animés les plus populaires */
+        $mostPopularAnime = $cache->get('most_popular_anime', function(ItemInterface $item) use($homeCallApiService){
+            $item->expiresAt(new \DateTime('tomorrow'));
+            return $homeCallApiService->getPopularAnime();
+        });
+
+        /* Le top 10 des meilleurs animés */
+        $topTenAnime = $cache->get('top_ten_anime', function(ItemInterface $item) use($homeCallApiService){
+            $item->expiresAt(new \DateTime('tomorrow'));
+            return $homeCallApiService->getTopTenAnime();
+        });
         
         return $this->render('home/index.html.twig', [
-            'dataBestAnimeThisSeason' => $homeCallApiService->getBestAnimeThisSeason(),
-            'dataNextSeasonAnime' => $homeCallApiService->getNextSeasonAnime(),
-            'dataPopularAnime' => $homeCallApiService->getPopularAnime(),
-            'dataTopTenAnime' => $homeCallApiService->getTopTenAnime(),
+            'dataBestAnimeThisSeason' => $bestAnimeThisSeason,
+            'dataNextSeasonAnime' => $nextSeasonAnime,
+            'dataPopularAnime' => $mostPopularAnime,
+            'dataTopTenAnime' => $topTenAnime,
         ]);
     }
 }
